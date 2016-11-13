ca36aa3caae1f00677bebc760c4e1b890ba5e0ff
n457 <n457.contact@gmail.com>
2016-11-13 19:39:46 +0100

    updated website to v.2.0

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
+  var $Input1 = document.getElementById('name-input-1');
+  var $Input2 = document.getElementById('name-input-2');
+  var $Input3 = document.getElementById('name-input-3');
+  var timerInputCheck;
 
-  var $Dot = $('.toggling-dot');
-  var timerDot;
+  forEach(document.getElementsByClassName('name-input'), ($Input) => {
+    $Input.addEventListener('input', () => {
+      clearTimeout(timerInputCheck);
+      timerInputCheck = setTimeout(function () {
+        if ($Input1.textContent === '4' && $Input2.textContent === '5' && $Input3.textContent === '7') {
+          window.open('https://n457.github.io/Uncolored/');
+        }
+        clearTimeout(timerInputCheck);
+      }, 10);
+    });
 
-  timerDot = setInterval(function () {
-    if ($Dot.hasClass('active')) {
-      $Dot.removeClass('active');
-    } else {
-      $Dot.addClass('active');
-    }
-  }, 500);
+    $Input.addEventListener('blur', () => {
+      if (! $Input.textContent) {
+        $Input.textContent = '_';
+      }
+    });
+  });
 
 });
